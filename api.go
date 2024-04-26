62807e2cb1b94ffbf7132217b6c8980d289b3818
AndresCampuzano <campuxano88@gmail.com>
2024-04-25 20:16:38 -0500

    add go docs on api.go

--- api.go
+++ api.go
@@ -9,11 +9,13 @@ import (
 	"net/http"
 )
 
+// APIServer represents an HTTP server for handling API requests.
 type APIServer struct {
 	listenAddr string
 	store      Storage
 }
 
+// NewAPIServer creates a new instance of APIServer.
 func NewAPIServer(listenAddr string, store Storage) *APIServer {
 	return &APIServer{
 		listenAddr: listenAddr,
@@ -21,27 +23,33 @@ func NewAPIServer(listenAddr string, store Storage) *APIServer {
 	}
 }
 
+// Run starts the API server and listens for incoming requests.
 func (s *APIServer) Run() {
 	router := mux.NewRouter()
 
 	router.HandleFunc("/account", makeHTTPHandlerFunc(s.handleAccount))
-	router.HandleFunc("/account/{id}", makeHTTPHandlerFunc(s.handleGetAccountByID))
+	router.HandleFunc("/account/{id}", makeHTTPHandlerFunc(s.handleAccountAndID))
 
 	log.Println("JSON API server running on port: ", s.listenAddr)
 
 	http.ListenAndServe(s.listenAddr, router)
 }
 
+// handleAccount handles requests related to account management.
+// It supports HTTP methods GET and POST.
 func (s *APIServer) handleAccount(w http.ResponseWriter, r *http.Request) error {
-	if r.Method == http.MethodGet {
+	switch r.Method {
+	case http.MethodGet:
 		return s.handleGetAccounts(w, r)
-	}
-	if r.Method == http.MethodPost {
+	case http.MethodPost:
 		return s.handleCreateAccount(w, r)
+	default:
+		return fmt.Errorf("unsupported method: %s", r.Method)
 	}
-	return fmt.Errorf("unsupported method: %s", r.Method)
 }
 
+// handleGetAccountByID handles requests to retrieve an account by ID.
+// It supports HTTP method GET.
 func (s *APIServer) handleGetAccounts(w http.ResponseWriter, r *http.Request) error {
 	accounts, err := s.store.GetAccounts()
 	if err != nil {
@@ -50,8 +58,11 @@ func (s *APIServer) handleGetAccounts(w http.ResponseWriter, r *http.Request) er
 	return WriteJSON(w, http.StatusOK, accounts)
 }
 
-func (s *APIServer) handleGetAccountByID(w http.ResponseWriter, r *http.Request) error {
-	if r.Method == http.MethodGet {
+// handleCreateAccount handles requests to create a new account.
+// It supports HTTP method POST.
+func (s *APIServer) handleAccountAndID(w http.ResponseWriter, r *http.Request) error {
+	switch r.Method {
+	case http.MethodGet:
 		id, err := getID(r)
 		if err != nil {
 			return err
@@ -63,15 +74,15 @@ func (s *APIServer) handleGetAccountByID(w http.ResponseWriter, r *http.Request)
 		}
 
 		return WriteJSON(w, http.StatusOK, account)
-	}
-
-	if r.Method == http.MethodDelete {
+	case http.MethodDelete:
 		return s.handleDeleteAccount(w, r)
+	default:
+		return fmt.Errorf("unsupported method: %s", r.Method)
 	}
-
-	return fmt.Errorf("unsupported method: %s", r.Method)
 }
 
+// handleDeleteAccount handles requests to delete an account.
+// It supports HTTP method DELETE.
 func (s *APIServer) handleCreateAccount(w http.ResponseWriter, r *http.Request) error {
 	createAccountRequest := new(CreateAccountRequest)
 	if err := json.NewDecoder(r.Body).Decode(createAccountRequest); err != nil {
@@ -94,6 +105,8 @@ func (s *APIServer) handleCreateAccount(w http.ResponseWriter, r *http.Request) 
 	return WriteJSON(w, http.StatusOK, createdAccount)
 }
 
+// handleTransfer handles requests to transfer funds between accounts.
+// It supports HTTP method POST.
 func (s *APIServer) handleDeleteAccount(w http.ResponseWriter, r *http.Request) error {
 	id, err := getID(r)
 	if err != nil {
@@ -107,22 +120,31 @@ func (s *APIServer) handleDeleteAccount(w http.ResponseWriter, r *http.Request) 
 	return WriteJSON(w, http.StatusOK, map[string]string{"deleted": id})
 }
 
+// handleTransfer handles requests to transfer funds between accounts.
+// TODO: finish
 func (s *APIServer) handleTransfer(w http.ResponseWriter, r *http.Request) error {
 	return nil
 }
 
+// WriteJSON writes the given data as JSON to the HTTP response with the provided status code.
+// It sets the "Content-Type" header to "application/json; charset=utf-8".
 func WriteJSON(w http.ResponseWriter, status int, v any) error {
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
 	w.WriteHeader(status)
 	return json.NewEncoder(w).Encode(v)
 }
 
+// apiFunc is a type representing a function that handles HTTP requests and returns an error.
 type apiFunc func(http.ResponseWriter, *http.Request) error
 
+// ApiError represents an error response in JSON format.
 type ApiError struct {
 	Error string `json:"error"`
 }
 
+// makeHTTPHandlerFunc creates an HTTP handler function from the given apiFunc.
+// It calls the provided function f to handle HTTP requests, and if an error occurs, it writes
+// the error response as JSON with status code http.StatusBadRequest.
 func makeHTTPHandlerFunc(f apiFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		if err := f(w, r); err != nil {
@@ -131,6 +153,8 @@ func makeHTTPHandlerFunc(f apiFunc) http.HandlerFunc {
 	}
 }
 
+// getID extracts the ID parameter from the URL path of the HTTP request r.
+// It returns the extracted ID and an error if the ID is invalid or not found in the request.
 func getID(r *http.Request) (string, error) {
 	id := mux.Vars(r)["id"]
 
